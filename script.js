e7992c57448df756d6ddf4941492d226955c62e8
Rohan Tandale <124422395+RohanTandale@users.noreply.github.com>
2026-01-12 12:35:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+let currentAudio = null;
+
+function playSound(soundName) {
+    if (currentAudio) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0;
+    }
+
+    currentAudio = new Audio(`sounds/${soundName}.mp3`);
+    currentAudio.play();
+}
+
+function stopSound() {
+    if (currentAudio) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0;
+    }
+}
